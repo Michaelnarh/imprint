0ad4ec3c4fe8020bd7c19df55fc1dd8e90c4e5b7
unknown <michaelnarh>
2022-06-30 15:38:00 +0000

    changed the host and debug to false

--- imprint/settings.py
+++ imprint/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m_zsiy3=$p-fn_=h^&+-3bbt6dn0rxq+2rdtx66huzm57=@hb3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
